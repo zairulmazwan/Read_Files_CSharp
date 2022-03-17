228c61c35b820ca1d0440844781f8a14ef5edf36
zm1142 <zm1142@STA-DK-400392>
2022-03-17 14:18:09 +0000

    Add project files.

---
+++ Files/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "C:\\Users\\zm1142\\source\\repos\\Files\\Files\\data.csv";
+
+            StreamReader reader = null;
+
+            if (File.Exists(fileName))
+            {
+                reader = new StreamReader(File.OpenRead(fileName));
+                List<string> data = new List<string>();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    
+                    foreach (var item in values)
+                    {
+                        data.Add(item);
+                        Console.Write(item);
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+            }
+       
+        }
+        
+    }
+}
